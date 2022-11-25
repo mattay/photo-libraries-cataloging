de7bb98944f4153f91659cd1b7342ba6c9d36916
Matthew Anderson <mattay.anderson@gmail.com>
2022-11-25 12:02:36 +1000

    Added sqlite to brew install list

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 echo 'Installing ExifTool'
-brew install ExifTool
+brew install ExifTool sqlite
 
 echo 'Installing python requirements'
 pipenv run pip install --requirement requirements.txt #> /dev/null
