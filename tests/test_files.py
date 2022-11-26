c4de37d9dcc3ea7d4569dc99b598853fac18c29f
Matthew Anderson <mattay.anderson@gmail.com>
2022-11-26 12:32:34 +1000

    Added first test

---
+++ tests/test_files.py
@@ -0,0 +1,17 @@
+import pytest
+from photologue.files import clean_name
+
+
+@pytest.mark.parametrize("test_file_paths, expected_image_name", [
+    ('/Volumes/Jedi/CLEANUP//Cars/2010/03/13/20100313-083911/IMG_1740 (1).jpg', {
+        'name': 'IMG_1740',
+        'path': '/Volumes/Jedi/CLEANUP//Cars/2010/03/13/20100313-083911/IMG_1740 (1).jpg',
+        'extention': '.jpg',
+        'thumbnail': False,
+        'copy in name': False,
+        'copy subfix': True,
+        'face': False
+    })
+])
+def test_clean_name(test_file_paths, expected_image_name):
+    assert clean_name(test_file_paths) == expected_image_name
