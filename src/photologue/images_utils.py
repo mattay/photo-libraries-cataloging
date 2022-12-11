9436ceb057f9603c9c6596b40b2549864c795a23
Matthew Anderson <mattay.anderson@gmail.com>
2022-12-11 16:39:08 +1000

    Pre-process files while grouping

--- src/photologue/images_utils.py
+++ src/photologue/images_utils.py
@@ -8,3 +8,35 @@ def original_date(date_time: str) -> object:
         date = date_time[:10]
     return date
 
+
+def group_files_by_image_date(files: list[dict]) -> dict:
+    """Organises files into a heiarchy of Images then date/time and files
+
+        Parameters
+        ----------
+        images : list[dict]
+            list of files
+
+        Returns
+        -------
+        dict
+            heireacy {image: {moment: [files]}}
+        """
+    collected: dict[str, dict] = {}
+
+    for file in files:
+        # clean up dates
+        file['image_date'] = original_date(file['date_time_original'])
+        file['mod_date'] = original_date(file['date_time_modifed'])
+        # Calculate if file has been modified
+        file['modified'] = 1 if file['image_date'] != file['mod_date'] else 0
+        file['copy'] = 1 if file['has_copy_in_subfix'] or file['has_copy_in_name'] else 0
+        # Add to Hhiarchy
+        i = collected.get(file['file_name'], {})
+        t = i.get(file['image_date'], [])
+        t.append(file)
+
+        i[file['image_date']] = t
+        collected[file['file_name']] = i
+
+    return collected
