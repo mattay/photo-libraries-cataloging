3df706836dfa856bba6e0f86c6407f91429f9a43
Matthew Anderson <mattay.anderson@gmail.com>
2023-09-26 12:43:08 +1000

    Relocated test for grouping functions

--- tests/test_clean_utils.py
+++ tests/test_clean_utils.py
@@ -1,71 +1,16 @@
 import pytest
-from photologue.clean_utils import group_by_raw_plus_checksums, group_by_paths
-
-DMC_LX3_P1000847 = [
-    {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/LaCie/_MASTER_Aperture_20190520/ForSale.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-    {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/LaCie/_MASTER_Aperture_20190520/ForSale.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'},
-    {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__1.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-    {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__1.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'},
-    {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__2.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-    {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__2.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'},
-    {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__3.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-    {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__3.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'},
-    {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ItemsForSale__4.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-    {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ItemsForSale__4.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'},
-]
-DMC_LX3_P1000847_group_by_paths = {
-    '/Volumes/LaCie/_MASTER_Aperture_20190520/ForSale.aplibrary/Masters/2010/02/28/20100228-211134': {
-        '.JPG': {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/LaCie/_MASTER_Aperture_20190520/ForSale.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-        '.RW2': {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/LaCie/_MASTER_Aperture_20190520/ForSale.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'}
-    },
-    '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__1.aplibrary/Masters/2010/02/28/20100228-211134': {
-        '.JPG': {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__1.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-        '.RW2': {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__1.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'}
-    },
-    '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__2.aplibrary/Masters/2010/02/28/20100228-211134': {
-        '.JPG': {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__2.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-        '.RW2': {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__2.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'}
-    },
-    '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__3.aplibrary/Masters/2010/02/28/20100228-211134': {
-        '.JPG': {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__3.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-        '.RW2': {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__3.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'}
-    },
-    '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ItemsForSale__4.aplibrary/Masters/2010/02/28/20100228-211134': {
-        '.JPG': {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ItemsForSale__4.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-        '.RW2': {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ItemsForSale__4.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'}
-    }
-}
-DMC_LX3_P1000847_raw_plus_checksum = {
-    'raw': {1414083411: [
-        {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/LaCie/_MASTER_Aperture_20190520/ForSale.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'},
-        {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__1.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'},
-        {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__2.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'},
-        {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__3.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'},
-        {'checksum': 1414083411, 'file_extention': '.RW2', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ItemsForSale__4.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.RW2'}
-    ]},
-    'jpg': {1487073708: [
-        {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/LaCie/_MASTER_Aperture_20190520/ForSale.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-        {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__1.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-        {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__2.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-        {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ForSale__3.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'},
-        {'checksum': 1487073708, 'file_extention': '.JPG', 'file_path': '/Volumes/Padawan/_Pictures/Aperture Library Collections/For Sale/ItemsForSale__4.aplibrary/Masters/2010/02/28/20100228-211134/P1000847.JPG'}
-    ]}
-}
+from photologue.clean.clean_utils import are_files_incrementing
 
 
-@pytest.mark.parametrize("test__group_by_paths, expected_pairs", [
-    (DMC_LX3_P1000847, DMC_LX3_P1000847_group_by_paths)
+@pytest.mark.parametrize("paths, expected", [
+    ([
+        {'file_name': 'G0010006', 'file_path': '/Volumes/Jedi/GoPro/2014-01-02 Home/HERO4 Silver/PHOTO_SEQUENCE 1/G0010006.JPG'},
+        {'file_name': 'G0010007', 'file_path': '/Volumes/Jedi/GoPro/2014-01-02 Home/HERO4 Silver/PHOTO_SEQUENCE 1/G0010007.JPG'}
+    ], True),
+    ([
+        {'file_name': '_IGP0671', 'file_path': '/Volumes/LaCie/_MASTER_Aperture_20190520/Brisbane.aplibrary/Masters/2011/01/13/20110113-103404/_IGP0671.PEF'},
+        {'file_name': '_IGP0672', 'file_path': '/Volumes/LaCie/_MASTER_Aperture_20190520/Brisbane.aplibrary/Masters/2011/01/13/20110113-103404/_IGP0672.PEF'}
+    ], True)
 ])
-def test__group_by_paths(test__group_by_paths, expected_pairs):
-    assert group_by_paths(test__group_by_paths) == expected_pairs
-
-
-@pytest.mark.parametrize("test__raw_plus_checksum, expected_pairs", [
-    ({}, ({
-        'raw': {},
-        'jpg': {}
-    })),
-    (DMC_LX3_P1000847_group_by_paths, DMC_LX3_P1000847_raw_plus_checksum)
-])
-def test__group_by_raw_plus_checksums(test__raw_plus_checksum, expected_pairs):
-    assert group_by_raw_plus_checksums(test__raw_plus_checksum) == expected_pairs
+def test__are_files_incrementing(paths, expected):
+    assert are_files_incrementing(paths) == expected
